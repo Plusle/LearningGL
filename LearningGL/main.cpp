4a2077237b7cc0fb85f94fa0b972780cd3caf441
Plusle <synchz2k@gmail.com>
2020-06-15 11:33:25 +0900

    template for new coding

--- LearningGL/main.cpp
+++ LearningGL/main.cpp
@@ -4,12 +4,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-
 #include "Shader.h"
 
 #include <iostream>
-
-
 
 // framebuffer size callback function
 //---------------------------------------------------------------
@@ -81,6 +78,7 @@ int main(int argc, char** argv) {
 	};
 
 	// draw indices
+	// lb -> lt -> rt -> rb
 	GLuint indices[] = {
 		0, 1, 2, 
 		2, 3, 0
@@ -92,6 +90,9 @@ int main(int argc, char** argv) {
 	glGenBuffers(1, &VBO);
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &EBO);
+
+	// bind VAO firstly ensure everything in control
+	// but the fact is only EBO binding behaivor and vertex attribute pointer saved
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
@@ -108,46 +109,51 @@ int main(int argc, char** argv) {
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 9, (void*)(sizeof(GLfloat) * 7));
 	glEnableVertexAttribArray(2);
 
+	// create textures
+	//---------------------------------------------------------------
 	GLuint tex_Background, tex_Face;
+	
+	// the texture var is established ralationship with parameters and texture data
+	// generate first texture: the background 'container', it's a rgb pic with file format of jpg, NO transparency
 	glGenTextures(1, &tex_Background);
 	glBindTexture(GL_TEXTURE_2D, tex_Background);
+
+	// warp setting
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	
+	// mipmap setting
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	
+	// flip the image because the origin in left bottom but image's in left top
 	stbi_set_flip_vertically_on_load(true);
+
+	// convert image into byte array
 	GLint width, height, nrChannels;
 	unsigned char* data1 = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
 	if (data1) {
+		//target: 2D, mipmap level: 0 for set manually, pic format, width, height, legacy burden 0 eternally, format, datatype, data
+		// note that the first GL_RGB is int while the latter is GLenum (for what?)
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data1);
+		
+		// set mipmap automatically otherwise do it manually
 		glGenerateMipmap(GL_TEXTURE_2D);
 	} else {
 		std::cout << "Failed to load background." << std::endl;
 	}
-	stbi_image_free(data1);
 
-	glGenTextures(1, &tex_Face);
-	glBindTexture(GL_TEXTURE_2D, tex_Face);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	unsigned char* data2 = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
-	if (data2) {
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data2);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else {
-		std::cout << "Failed to load face." << std::endl;
-	}
-	stbi_image_free(data2);
+	// good practice free the memory immediately
+	stbi_image_free(data1);
 
 	// shader program initializing
 	//---------------------------------------------------------------
 	Shader myProgram("vertex.gls", "fragment.gls");
-	myProgram.use();
-	myProgram.setInt("background", 0);
-	myProgram.setInt("face", 1);
+
+	// when only 1 sampler, some of graphics card can do this implicitly
+	//myProgram.use();
+	//myProgram.setInt("background", 0);			
+
 
 	// main loop
 	//---------------------------------------------------------------
@@ -157,10 +163,10 @@ int main(int argc, char** argv) {
 		glClearColor(0.0f, 0.1f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// the implicit feature can make us omit the following statment.
 		glActiveTexture(GL_TEXTURE0);
+
 		glBindTexture(GL_TEXTURE_2D, tex_Background);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, tex_Face);
 
 		glBindVertexArray(VAO);
 		myProgram.use();
@@ -175,6 +181,8 @@ int main(int argc, char** argv) {
 	myProgram.kill();
 	glDeleteBuffers(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteTextures(1, &tex_Background);
+	glDeleteTextures(1, &tex_Face);
 
 	glfwTerminate();
 	return 0;
