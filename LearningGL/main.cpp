45dbb749e7b876f3e927f29ca09d01fbd6f0423f
Plusle <synchz2k@gmail.com>
2020-07-17 07:41:53 +0900

    depth test

--- LearningGL/main.cpp
+++ LearningGL/main.cpp
@@ -5,10 +5,11 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <stb_image.h>
 
 #include "Shader.h"
 #include "FPSCamera.h"
-#include "Model.h"
+
 
 #include <iostream>
 
@@ -30,6 +31,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void process_input(GLFWwindow* window);
+GLuint load_texture(char const* path);
 
 int main(int argc, char** argv) {
 
@@ -58,20 +60,99 @@ int main(int argc, char** argv) {
 
 	// no cursor window
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glEnable(GL_DEPTH_TEST);
 
 	// load opengl function address
 	//---------------------------------------------------------------
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cout << "Failed to load OpenGL function" << std::endl;
-		glfwTerminate();
+		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
 
-	Shader shader = Shader("model.vert", "model.frag");
+	// vertrices
+	float cubeVertices[] = {
+		// positions          // texture Coords
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
-	Model nano = Model("nano/nanosuit.obj");
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
 
-	glEnable(GL_DEPTH_TEST);
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
+	float planeVertices[] = {
+		// positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
+		 5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+		-5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
+		-5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+
+		 5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+		-5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+		 5.0f, -0.5f, -5.0f,  2.0f, 2.0f
+	};
+
+	GLuint cube_buffer, cube_array;
+	glGenBuffers(1, &cube_buffer);
+	glGenVertexArrays(1, &cube_array);
+	glBindVertexArray(cube_array);
+	glBindBuffer(GL_ARRAY_BUFFER, cube_buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void*)(3 * sizeof(float)));
+
+	GLuint floor_buffer, floor_array;
+	glGenBuffers(1, &floor_buffer);
+	glGenVertexArrays(1, &floor_array);
+	glBindVertexArray(floor_array);
+	glBindBuffer(GL_ARRAY_BUFFER, floor_buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void*)(3 * sizeof(float)));
+
+	GLuint cube_texture = load_texture("marble.jpg");
+	GLuint floor_texture = load_texture("floor.png");
+
+	Shader shader("depth.vert", "depth.frag");
+	shader.use();
+	shader.setInt("texture", 0);
 
 	// main loop
 	//---------------------------------------------------------------
@@ -81,16 +162,26 @@ int main(int argc, char** argv) {
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		glm::mat4 model = glm::mat4(1.0f);
-		glm::mat4 projection = glm::perspective(camera.getFOV(), (float)(SCR_WIDTH / SCR_HEIGHT), 0.01f, 100.0f);
-		glm::mat4 view = camera.getView();
-
 		shader.use();
-		shader.setMat4("model", model);
+		glm::mat4 view = camera.getView();
+		glm::mat4 projection = glm::perspective(camera.getFOV(), (float)(SCR_WIDTH) / (float)(SCR_HEIGHT), 0.1f, 200.0f);
+
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 cube1 = glm::translate(model, glm::vec3(1.0f, 0.0f, 3.0f));
+		glm::mat4 cube2 = glm::translate(model, glm::vec3(2.0f, 0.0f, 1.0f));
+		
 		shader.setMat4("view", view);
 		shader.setMat4("projection", projection);
-		nano.Draw(shader);
 
+		glBindVertexArray(floor_array);
+		shader.setMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glBindVertexArray(cube_array);
+		shader.setMat4("model", cube1);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		shader.setMat4("model", cube2);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -155,4 +246,43 @@ void process_input(GLFWwindow* w) {
 	if (glfwGetKey(w, GLFW_KEY_D) == GLFW_PRESS)
 		camera.walk(CAMERA_MOVE_RIGHT, deltaTime);
 
+}
+
+// function for loading texture temporarily 
+//---------------------------------------------------------------
+GLuint load_texture(char const* path)
+{
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+
+	GLint width, height, nrComponents;
+	GLubyte* data = stbi_load(path, &width, &height, &nrComponents, 0);
+	if (data)
+	{
+		GLenum format;
+		if (nrComponents == 1)
+			format = GL_RED;
+		else if (nrComponents == 3)
+			format = GL_RGB;
+		else if (nrComponents == 4)
+			format = GL_RGBA;
+
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		stbi_image_free(data);
+	}
+	else
+	{
+		std::cout << "Texture failed to load at path: " << path << std::endl;
+		stbi_image_free(data);
+	}
+
+	return textureID;
 }
